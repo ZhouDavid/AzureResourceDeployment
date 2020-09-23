b3cf953328a2b8b217c4289375cfdfde2a5e55f7
David Zhou <zhouj@microsoft.com>
2020-09-23 15:49:20 -0700

    remove secrets

--- Deploy KeyVault/PushKeyToKeyVault.ps1
+++ Deploy KeyVault/PushKeyToKeyVault.ps1
@@ -12,8 +12,7 @@ param(
     $StorageAccountName
 )
 
-# $storageAccountKey = $(Get-AzStorageAccountKey -ResourceGroupName $StorageResourceGroup -AccountName $StorageAccountName)[0].Value
-$storageAccountKey = "LyN6vv+HfJF7PZgJxOP9BQIINKV0QpZ0tGx4Ge5uQgffsdzYx8etoinBkt3Yyg4Cs8TJh0FKDP04v/gEPwKXsw=="
+$storageAccountKey = $(Get-AzStorageAccountKey -ResourceGroupName $StorageResourceGroup -AccountName $StorageAccountName)[0].Value
 $storageAccountKey = ConvertTo-SecureString $storageAccountKey -AsPlainText -Force
 $secretName = "$StorageAccountName-AccessKey"
 Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $secretName -SecretValue $storageAccountKey
--- Deploy KeyVault/PushRackTelemetryKeyToKeyVault.ps1
+++ Deploy KeyVault/PushRackTelemetryKeyToKeyVault.ps1
@@ -7,20 +7,20 @@ foreach ($regionSpec in $jsonObj.RegionSpecs){
         $regionAlias = $regionSpec.RegionAlias
         $env = $regionSpec.Environment
         $keyVaultName = "spkl-kv-adb-$env-$regionAlias-1"
-        # # push adl key to adb keyvault
-        # $storRg = "spkl-adl-rt-$env-$regionAlias-0"
-        # $stor = "spkladlrt$env$regionAlias"+"x0"
-        # Write-Host $stor
-        # .\PushKeyToKeyVault.ps1 -KeyVaultName $keyVaultName -StorageResourceGroup $storRg -StorageAccountName $stor
+        # push adl key to adb keyvault
+        $storRg = "spkl-adl-rt-$env-$regionAlias-0"
+        $stor = "spkladlrt$env$regionAlias"+"x0"
+        Write-Host $stor
+        .\PushKeyToKeyVault.ps1 -KeyVaultName $keyVaultName -StorageResourceGroup $storRg -StorageAccountName $stor
 
-        # # push adladx key to adb keyvault
-        # $storRg = "spkl-adl-adx-$env-$regionAlias-0"
-        # $stor = "spkladladx$env$regionAlias"+"x0"
-        # Write-Host $stor
-        # .\PushKeyToKeyVault.ps1 -KeyVaultName $keyVaultName -StorageResourceGroup $storRg -StorageAccountName $stor
+        # push adladx key to adb keyvault
+        $storRg = "spkl-adl-adx-$env-$regionAlias-0"
+        $stor = "spkladladx$env$regionAlias"+"x0"
+        Write-Host $stor
+        .\PushKeyToKeyVault.ps1 -KeyVaultName $keyVaultName -StorageResourceGroup $storRg -StorageAccountName $stor
         
         # push global adl key to adb keyvault
-        .\PushKeyToKeyVault.ps1 -KeyVaultName $keyVaultName -StorageResourceGroup "spkl-adl-prod-gbl-0" -StorageAccountName "spkladlprodgblx0"
+        # .\PushKeyToKeyVault.ps1 -KeyVaultName $keyVaultName -StorageResourceGroup "spkl-adl-prod-gbl-0" -StorageAccountName "spkladlprodgblx0"
     }
     catch{
         Write-Error $SubId
